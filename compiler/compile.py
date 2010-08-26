b2df00936689e88c62cb541ae5e0c0d4ba25acce
Swizec Teller <swizec@swizec.com>
2010-08-27 00:32:48 +0200

    rewriting the compiler almost all previously implemented functionality

--- compiler/compile.py
+++ compiler/compile.py
@@ -5,53 +5,102 @@ import parsers
 
 TRANSLATIONS = {'.': 'dot'}
 
+#def parse(code):
+#    arguments = []
+#    string = []
+#    in_string = False
+#    dont_pass = False
+#    lines = []
+
+#    def _parse(element):
+#        try:
+#            parser = getattr(parsers, element)
+#            return [parser.main(arguments)]
+#        except AttributeError:
+#            try:
+#                return _parse(TRANSLATIONS[element])
+#            except KeyError:
+#                return arguments+[element]
+
+#    for element in code:
+#        if element == '':
+#            lines += arguments
+#            arguments = []
+#            continue
+
+#        if element[-1:] in TRANSLATIONS.keys() and not in_string:
+#            arguments = _parse(element[-1:])
+#            element = element[:-1]
+
+#        if element[-1:] == '"':
+#            in_string = True
+        
+#        if not in_string:
+#            arguments = _parse(element)
+#        else:
+#            string.append(element)
+#            if element[0] == '"':
+#                in_string = False
+#                arguments.append(' '.join([word for word in reversed(string)]))
+#                string = []
+
+#    lines += arguments
+        
+#    return reversed(lines)
+
 def parse(code):
-    arguments = []
-    string = []
-    in_string = False
-    dont_pass = False
-    lines = []
+    def _string(code):
+        s = []
+        for word in code:
+            s.append(word)
+            if word[-1:] == '"':
+                return (' '.join(s), len(s))
+        raise ParsingError("Unterminated string")
 
-    def _parse(element):
-        try:
-            parser = getattr(parsers, element)
-            return [parser.main(arguments)]
-        except AttributeError:
-            try:
-                return _parse(TRANSLATIONS[element])
-            except KeyError:
-                return arguments+[element]
+    def _consume(code, n):
+        stack = []
 
-    for element in code:
-        if element == '':
-            lines += arguments
-            arguments = []
-            continue
+        if n == 'Inf':
+            n = len(code)
 
-        if element[-1:] in TRANSLATIONS.keys() and not in_string:
-            arguments = _parse(element[-1:])
-            element = element[:-1]
+        i = 0
+        while i < n:
+            element = code[i]
 
-        if element[-1:] == '"':
-            in_string = True
-        
-        if not in_string:
-            arguments = _parse(element)
-        else:
-            string.append(element)
+            if element == '':
+                return (stack, i)
+            
             if element[0] == '"':
-                in_string = False
-                arguments.append(' '.join([word for word in reversed(string)]))
-                string = []
+                (element, offset) = _string(code[i:])
+                i += offset
 
-    lines += arguments
-        
-    return reversed(lines)
+            try:
+                parser = getattr(parsers, element)
+            except AttributeError:
+                pass
+            else:
+                (args, offset) = _consume(code[i+1:], parser.N_ARGS)
+                i += offset
+                element = parser.main(args)
+
+            stack.append(element)
+            i += 1
+
+        return (stack, i)
+            
+    lines = []
+    while len(code) > 0:
+        (line, offset) = _consume(code, len(code))
+        lines += line
+        code = code[offset+1:]
+
+    return lines
+    
 
 def compile(source, target=None):
     f = file(source, 'r')
 
-    code = reversed(reduce(lambda a,b: a+b.strip().split(' ') if b[0] != '#' else a, f, []))
+    code = reduce(lambda a,b: a+b.strip().split(' ') if b[0] != '#' else a, f, [])
     result = parse(code)
 
     f.close()
@@ -69,6 +118,10 @@ def save(result, target):
     f.writelines([''.join([line, "\n"]) for line in result])
 
     f.close()
+
+
+class ParsingError(Exception):
+    pass
 
 
 if __name__=='__main__':
--- compiler/parsers/say.py
+++ compiler/parsers/say.py
@@ -1,5 +1,7 @@
 
 from parsers.string import main as string
 
+N_ARGS = 'Inf'
+
 def main(args):
     return 'ex.say.main({0})'.format(string(args))
